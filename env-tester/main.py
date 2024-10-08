623aa5033b4abc0776e6c7f5d4891c896353b81d
Sreya-Mandalika <sreyamandalika@gmail.com>
2024-10-08 13:51:38 -0400

    Delete env-tester directory

--- env-tester/main.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-#setting up MongoDB and collections
-load_dotenv(r"C:\Users\Sreya Mandalika\coding-projects\projects-in-programming\env-tester\.env")
-
-hello = print(os.getenv("test"))
